5ef87a4b0054339327e9ccbbeefc04e7477374da
Kashan <kashanali1912@gmail.com>
2024-10-29 11:54:31 +0500

    even odd

--- code.cpp
+++ code.cpp
@@ -3,16 +3,18 @@ using namespace std;
 
 int main(){
 
-    int age;
-    cout<<"Enter Age :";
-    cin>>age;
-    if(age >= 18){
-        cout<<"Congratulations you are eligible for for Driving license"<<endl;
+    int n;
+    if(n%2==0){
+        cout<<"even"<<endl;
+
     }
     else{
-        cout<<"Sorry you are underage"<<endl;
+        cout<<"odd"<<endl;
+    }
+
+
     }
     
-}
+
 // Boilerplate Code
 //g++ -o code code.cpp && code
