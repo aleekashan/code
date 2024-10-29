2b61ec89d86d96baced2721962586ecab63eefc7
Kashan <kashanali1912@gmail.com>
2024-10-29 13:45:22 +0500

    While Loop

--- code.cpp
+++ code.cpp
@@ -3,23 +3,31 @@ using namespace std;
 
 int main(){
 
-    int marks;
-    cout<<"Enter Marks : ";
-    cin>>marks;
-    if(marks>=90){
-        cout<<"Grade A+";
+    // int marks;
+    // cout<<"Enter Marks : ";
+    // cin>>marks;
+    // if(marks>=90){
+    //     cout<<"Grade A+";
     
-    }
-    else if (marks>=80 && marks>=70 )
+    // }
+    // else if (marks>=80 && marks>=70 )
+    // {
+    //     cout<<"Grade A";
+    // }
+    // else if (marks>=60){
+    //     cout<<"Grade C";
+    // }
+    // else{
+    //     cout<<"Sorry apply next year";
+    // }
+    int n = 5000;
+    int count = 1;
+    while (count <= n)
     {
-        cout<<"Grade A";
+       cout<< count << " ";
+       count++;
     }
-    else if (marks>=60){
-        cout<<"Grade C";
-    }
-    else{
-        cout<<"Sorry apply next year";
-    }
+    
     
 
 
