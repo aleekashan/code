d36abe1a76297caa738bdf71c23b10b168084d99
Kashan <kashanali1912@gmail.com>
2024-10-29 11:47:49 +0500

    if else

--- code.cpp
+++ code.cpp
@@ -3,8 +3,16 @@ using namespace std;
 
 int main(){
 
-    int a = 10;
-    int b = ++a;
-    cout <<"a = "<< b << endl;
+    int age;
+    cout<<"Enter Age :";
+    cin>>age;
+    if(age >= 18){
+        cout<<"Congratulations you are eligible for for Driving license"<<endl;
+    }
+    else{
+        cout<<"Sorry you are underage"<<endl;
+    }
+    
 }
 // Boilerplate Code
+//g++ -o code code.cpp && code
